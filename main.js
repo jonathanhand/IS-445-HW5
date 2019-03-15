c9f909b460f2f82a83be29c2350c671e6f2de683
jonathanhand <hand.jonathan.13@gmail.com>
2019-03-14 21:25:43 -0700

    changed to one function

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 const keyboardInfo = e => {
   console.log(`${e.keyCode}`);
   if (`${e.keyCode}` === "107") {
-    addAir();
+    changeAir(1);
   } else if (`${e.keyCode}` === "109") {
-    takeAir();
+    changeAir(-1);
   }
 };
-var balloonElem = document.getElementById("balloon");
-document.addEventListener("keyup", keyboardInfo);
-function addAir() {
+function changeAir(key) {
   var currentSize = balloonElem.style.fontSize;
-  var newSize = parseInt(currentSize) * 1.15;
+  var newSize = 100;
+  if (key === 1) {
+    newSize = parseInt(currentSize) * 1.15;
+  } else {
+    newSize = parseInt(currentSize) * 0.85;
+  }
   balloonElem.style.fontSize = `${newSize}` + "%";
 }
 
-function takeAir() {
-  var currentSize = balloonElem.style.fontSize;
-  var newSize = parseInt(currentSize) * 0.85;
-  balloonElem.style.fontSize = `${newSize}` + "%";
-}
- 
+var balloonElem = document.getElementById("balloon");
+document.addEventListener("keyup", keyboardInfo);
