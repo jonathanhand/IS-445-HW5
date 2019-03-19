fec915f70488cf765c289fbf3feb454df2927b1e
jonathanhand <hand.jonathan.13@gmail.com>
2019-03-19 10:22:16 -0700

    added explosion and done

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 const keyboardInfo = e => {
   console.log(`${e.keyCode}`);
   if (`${e.keyCode}` === "107") {
-    changeAir(1);
+    changeAir(15);
   } else if (`${e.keyCode}` === "109") {
-    changeAir(-1);
+    changeAir(-15);
   }
 };
-function changeAir(key) {
-  var currentSize = balloonElem.style.fontSize;
-  var newSize = 100;
-  if (key === 1) {
-    newSize = parseInt(currentSize) * 1.15;
+
+function changeAir(key, currentSize) {
+  var currentSize = parseInt(balloonElem.style.fontSize);
+  var newSize = currentSize + key;
+  if (newSize <= 0) {
+    balloonElem.innerHTML = "Done";
+    balloonElem.style.fontSize = "60px";
+    document.removeEventListener("keyup", keyboardInfo);
+  } else if (newSize >= 60) {
+    balloonElem.innerHTML = "💥";
+    document.removeEventListener("keyup", keyboardInfo);
   } else {
-    newSize = parseInt(currentSize) * 0.85;
+    balloonElem.style.fontSize = `${newSize}` + "px";
   }
-  balloonElem.style.fontSize = `${newSize}` + "%";
 }
 
 var balloonElem = document.getElementById("balloon");
